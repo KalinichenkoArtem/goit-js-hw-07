5b22e041d786877ad12911beca729c3377670bc0
KalinichenkoArtem <110483657+KalinichenkoArtem@users.noreply.github.com>
2022-12-24 21:27:14 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,47 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const cardsMarkup = createGalleryItemsMarkup(galleryItems);
+
+// Render
+
+gallery.insertAdjacentHTML('afterbegin', cardsMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"/>
+            </a>
+        </div>`;
+}).join("");
+};
+
+// Render
+
+// addEventListener
+
+gallery.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+    
+    const modalImage = event.target.getAttribute("data-source");
+
+    const instance = basicLightbox.create(`<img
+                src="${modalImage}">`);
+
+    instance.show();    
+};
+
+// addEventListener
